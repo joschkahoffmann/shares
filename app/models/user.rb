69c2788b5a307fba2f77a165412878e922407ae7
joschkahoffmann <joschkahoffmann@gmail.com>
2016-09-10 18:51:58 +1200

    added user_stocks model written create method in user_stocks_controller and added add-to-portfolio-link

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  	
+  	has_many :user_stocks
+	has_many :stocks, through: :user_stocks
+
+
+
+
+
 end
