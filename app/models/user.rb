2b1e3efb886d6e230e67bf30084e58bdd442788f
joschkahoffmann <joschkahoffmann@gmail.com>
2016-09-14 10:48:30 +1200

    added friendship model and many-to-many-association

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   	
   	has_many :user_stocks
 	has_many :stocks, through: :user_stocks
+	has_many :friendships
+	has_many :friends, through: :friendships
 
 
 	def can_add_stock?(ticker_symbol)
